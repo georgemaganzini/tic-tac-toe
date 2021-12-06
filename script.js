a89d467fe197c613633a05964f5dc9447b432c0a
georgemaganzini <hyped@teamliquid.com>
2021-12-06 11:03:27 -0800

    Scaffold out sections

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('script js for project');
+
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+
